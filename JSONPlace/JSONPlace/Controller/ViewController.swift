a169037bf52f16c603e96be01009f2e7f9914d05
Jordi Milla Catalán <jb.90@hotmail.es>
2020-11-25 18:49:28 +0100

    Create tableView, tableViewCell and link them.

--- JSONPlace/JSONPlace/Controller/ViewController.swift
+++ JSONPlace/JSONPlace/Controller/ViewController.swift
@@ -6,15 +6,30 @@
 //  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import Moya
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 
 }
 
